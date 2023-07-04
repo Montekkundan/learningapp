b3b92582e4410d69f188d3efee9b7a3fa9c7f344
Montek <87750128+Montekkundan@users.noreply.github.com>
2023-07-04 04:14:12 -0700

    Update validateRequest.ts

--- server/src/middleware/validateRequest.ts
+++ server/src/middleware/validateRequest.ts
@@ -2,12 +2,15 @@ import { Request, Response, NextFunction } from 'express';
 import { ZodType } from 'zod';
 
 export function validateRequest(schema: ZodType<any>) {
-  return (req: Request, res: Response, next: NextFunction) => {
-    try {
-      schema.parse(req.body);
-      next();
-    } catch (error: any) {
-      res.status(400).send(error.errors);
-    }
-  };
-}
+    return (req: Request, res: Response, next: NextFunction) => {
+      console.log('Inside validateRequest middleware', req.body);
+      try {
+        schema.parse(req.body);
+        next();
+      } catch (error: any) {
+        console.log('Validation error:', error);
+        res.status(400).send(error.errors);
+      }
+    };
+  }
+  
